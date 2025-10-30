423ccd7d0b930c5c85e80b5b0850cbe25db10ad0
Eridani Alcantar <ealcantar@utexas.edu>
2025-10-30 12:44:58 -0500

    Added prime_numbers test.

---
+++ tests/test_prime_numbers.py
@@ -0,0 +1,7 @@
+from src import prime_numbers
+
+def test_summation():
+    assert prime_numbers.is_prime(4) == False
+    assert prime_numbers.is_prime(13) == True
+    assert prime_numbers.is_prime(7019) == True
+    assert prime_numbers.is_prime(2) == True
