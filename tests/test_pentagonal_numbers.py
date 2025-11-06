7ade0ca91de65cfd0c905d1d5b6666cb4015a0eb
Eridani Alcantar <eridani.alcantar@springbranchisd.com>
2025-11-06 17:36:20 -0600

    Update test_pentagonal_numbers.py
    Fixed naming error.

--- tests/test_pentagonal_numbers.py
+++ tests/test_pentagonal_numbers.py
@@ -2,8 +2,8 @@ from src import pentagonal_numbers
 
 
 def test_pentagonal_numbers():
-    assert getPentagonalNumber(1) == 1
-    assert getPentagonalNumber(2) == 5
-    assert getPentagonalNumber(3) == 12
-    assert getPentagonalNumber(36) == 1926
-    assert getPentagonalNumber(46) == 3151
+    assert pentagonal_numbers.getPentagonalNumber(1) == 1
+    assert pentagonal_numbers.getPentagonalNumber(2) == 5
+    assert pentagonal_numbers.getPentagonalNumber(3) == 12
+    assert pentagonal_numbers.getPentagonalNumber(36) == 1926
+    assert pentagonal_numbers.getPentagonalNumber(46) == 3151
